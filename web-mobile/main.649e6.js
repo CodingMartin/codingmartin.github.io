7edc2ad943499d30e40860c71dacd01694f816d3
martincheng91 <18670937083@163.com>
2018-09-20 17:18:11 +0800

    上传

--- web-mobile/main.649e6.js
+++ web-mobile/main.649e6.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.e2d03.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.a49cc.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -166,7 +166,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.bc384.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.649e6.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -190,7 +190,7 @@ if (false) {
     window.boot();
 }
 else if (window.jsb) {
-    require('src/settings.221a1.js');
+    require('src/settings.b45f8.js');
     require('src/cocos2d-jsb.js');
     require('jsb-adapter/engine/index.js');
     window.boot();
