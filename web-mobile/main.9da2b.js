7a718529febc912d51c554014054537a266e94cb
martincheng91 <18670937083@163.com>
2018-09-21 12:09:32 +0800

    cmm

--- web-mobile/main.9da2b.js
+++ web-mobile/main.9da2b.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.a49cc.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.9662d.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -166,7 +166,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.649e6.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.9da2b.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -190,7 +190,7 @@ if (false) {
     window.boot();
 }
 else if (window.jsb) {
-    require('src/settings.b45f8.js');
+    require('src/settings.20017.js');
     require('src/cocos2d-jsb.js');
     require('jsb-adapter/engine/index.js');
     window.boot();
